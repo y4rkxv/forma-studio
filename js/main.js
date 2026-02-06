bfc8a1c96fabc693eab8d7517e852068d4a8238d
Yaroslav <yaroslavkovalc03@gmail.com>
2026-02-06 17:12:02 +0200

    feat: add projects section with swiper slider

--- js/main.js
+++ js/main.js
@@ -4,3 +4,24 @@ const menu = document.querySelector('.menu__list');
 menuBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
+
+const swiper = new Swiper('.projects__slider', {
+  // Optional parameters
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.projects__arrow-next',
+    prevEl: '.projects__arrow-prev',
+  },
+});
